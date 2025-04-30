48cae5a3ed2eceae7c158c2adae45fd068ad23b3
Neagz <netars2013@gmail.com>
2025-04-30 16:01:24 +0300

    Верстка проекта

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+
+export { Button } from './button/button.ts'
+export { Link } from './link/link.ts'
+export { Input } from './input/input.ts'
+
+/*
+export { ListCat } from './list-cats'
+export { CatCard} from './cat-card'
+export { FormWrapper } from './form-wrapper'
+export { FormElement } from './form-element'
+export { Dialog } from './dialog'
+export { RemoveCatDialog } from './removeCatDialog'
+ */
---
+++ src/pages/profile_password/index.ts
@@ -0,0 +1,2 @@
+import './profile_password.css'
+export { default as ProfilePasswordPage } from './profile_password.hbs?raw';
